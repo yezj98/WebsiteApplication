8ab2907556adf35ca701a030c459b7886f3bd410
Yap <yezj98@gmail.com>
2020-03-10 19:13:38 +0800

    Website

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.ClipData;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,11 +26,12 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView.Adapter adapter;
     RecyclerView.LayoutManager layoutManager;
     ArrayList<ItemActivity> itemList;
-    String website;
+    String website, mWebsiteTitle;
 
     EditText insert_edit;
     Button insert_button;
-
+    ImageView imageView;
+    private int PermissionCode = 123;
 
 
     @Override
@@ -32,7 +41,11 @@ public class MainActivity extends AppCompatActivity {
 
         insert_button = findViewById(R.id.button);
         insert_edit = findViewById(R.id.editText);
+        imageView = findViewById(R.id.imageView1);
+        Permission();
 
+        Glide.with(this).load("http://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/1200px-Apple_logo_black.svg.png")
+                .dontAnimate().into(imageView);
 
 
         create();
@@ -48,22 +61,31 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
-
-
-
-
     }
 
-    public void addItem (String name) {
-        itemList.add(new ItemActivity(R.drawable.ic_android, "title", name));
+    public void addItem(String name) {
+
+        itemList.add(new ItemActivity(R.drawable.ic_android, websiteTitle(name), name));
     }
 
-    public void create () {
+    public static String websiteTitle (String url) {
+        URL mURL = null;
+        try {
+            mURL = new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        String domain = mURL.getHost();
+        return domain.startsWith("www.") ? domain.substring(4) : domain; // set the condition, if the domain start with "www." then return
+    }
+
+    public void create() {
         itemList = new ArrayList<>();
-        itemList.add(new ItemActivity(R.drawable.ic_android, "The title", "website"));
-        itemList.add(new ItemActivity(R.drawable.ic_android, "The title", "website"));
-        itemList.add(new ItemActivity(R.drawable.ic_android, "The title", "website"));
+
+
+        itemList.add(new ItemActivity(R.drawable.ic_android, websiteTitle("https://www.channelnewsasia.com"), "https://www.channelnewsasia.com"));
+        itemList.add(new ItemActivity(R.drawable.ic_android, websiteTitle("https://sg.yahoo.com"), "https://sg.yahoo.com"));
+        itemList.add(new ItemActivity(R.drawable.ic_android, websiteTitle("https://www.google.com"), "https://www.google.com"));
     }
 
     public void build() {
@@ -76,5 +98,15 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
 
 
+    }
+
+    private void Permission () {
+        String [] permission ={Manifest.permission.INTERNET};
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET)!=PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,permission ,PermissionCode);
+        }
+        else {
+
+        }
     }
 }
